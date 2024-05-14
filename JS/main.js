975644ac56996873186228a5d0f725ae1957d9fb
Hiwotgebre <hiwotg173@gmail.com>
2024-05-13 22:03:50 -0400

    Add functionality to dynamically append tems to the list

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,12 @@
-document.getElementById('itemForm').addEventListener('submit', function(event){
+document.getElementById('itemTemplate').addEventListener('submit', function(event){
     event.preventDefault();
-    const itemInput = document.getElementById('itemInput');
-    if (itemInput.value) {
+    const itemEntry = document.getElementById('itemEntry');
+    if (itemEntry.value) {
         const newItem = document.createElement(li);
-        newItem.textContent = itemInput.value;
+        newItem.textContent = itemEntry.value;
         document.getElementById('itemList').appendChild(newItem);
-        itemInput.value = ''; //Clear input after adding
+        itemEntry.value = ''; //Clear input after adding
     }
 });
+
+
