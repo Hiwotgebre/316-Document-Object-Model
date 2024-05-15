041921309de1d48d1fe7f4e00a3d5dd49fe60c97
Hiwotgebre <hiwotg173@gmail.com>
2024-05-14 20:23:10 -0400

    Deleting duplicate code

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,4 @@
-document.getElementById('itemTemplate').addEventListener('submit', function(event){
-    event.preventDefault();
-    const itemEntry = document.getElementById('itemEntry');
-    if (itemEntry.value) {
-        const newItem = document.createElement(li);
-        newItem.textContent = itemEntry.value;
-        document.getElementById('itemList').appendChild(newItem);
-        itemEntry.value = ''; //Clear input after adding
-    }
-});
+
 document.getElementById('itemTemplate').addEventListener('submit', function(event) {
     event.preventDefault();
     const itemEntry = document.getElementById('itemEntry');
